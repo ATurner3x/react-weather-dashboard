e09c239cf2bbf33e2925e9ef887ac6c54564ac92
Amadeus <a.turner3x@gmail.com>
2023-10-30 22:07:24 -0400

    add ionic to weather tab

--- src/pages/weatherTab.tsx
+++ src/pages/weatherTab.tsx
@@ -11,3 +11,40 @@ import {
   IonIcon,
 } from '@ionic/react';
 
+import { sunny } from 'ionicons/icons'; // Import icon here
+
+const WeatherTab: React.FC = () => {
+  // Weather Fetching Logic
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Weather Dashboard</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        {/* Display weather data here */}
+        <IonList>
+          <IonItem>
+            <IonIcon slot="start" icon={sunny} />
+            <IonLabel>Temperature: XX°C</IonLabel>
+          </IonItem>
+          {/* Add more weather information items */}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default WeatherTab;
