0deb0e6ad7d32dedf7a85dc5941347fbc0fa6c1f
Amadeus <a.turner3x@gmail.com>
2023-10-30 21:59:35 -0400

    added weatherTab.tsx and imports

---
+++ src/pages/weatherTab.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonLabel,
+  IonList,
+  IonItem,
+  IonIcon,
+} from '@ionic/react';
+
